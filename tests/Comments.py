809736409d147a480e0dbd65cbe5d082dacc9407
YuCheng Hu <yucheng.hu@insight.com>
2021-03-16 15:58:40 -0400

    Python Comments Example

---
+++ tests/Comments.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Python Comments Example
+# Author - yucheng.hu@insight.com
+
+
+print("Hello, YuCheng!")  # Python line comment
+
+# Python line comment
+print("Hello, YuCheng!")
+
+# Python multiple line comment - LINE 1
+# Python multiple line comment - LINE 2
+print("Hello, YuCheng!")
+
+'''
+Python multiple line comment - LINE 1
+Python multiple line comment - LINE 2
+'''
+print("Hello, YuCheng!")
+
+"""
+Python multiple line comment - LINE 1
+Python multiple line comment - LINE 2
+"""
+print("Hello, YuCheng Hu!")
